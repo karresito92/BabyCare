c11781d221bacbc8bd36932e9ea40c6a4bfdee4a
karresito92 <acarretero08@gmail.com>
2026-01-08 18:34:04 +0100

    Fix register with auto-login and Google Fonts

--- frontend_dist/flutter_bootstrap.js
+++ frontend_dist/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"c29809135135e262a912cf583b2c90deb9ded6
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "758398360"
+    serviceWorkerVersion: "3490690794"
   }
 });
